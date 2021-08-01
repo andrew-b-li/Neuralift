dbce1fdde03d652d38859b9ed1142b3909c31e43
Andrew Li <31107424+andrew-b-li@users.noreply.github.com>
2021-08-01 00:53:51 -0600

    Added a basic power bar

--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -3,9 +3,15 @@ import FpsText from '../objects/fpsText'
 
 export default class MainScene extends Phaser.Scene {
   fpsText
+  fullWidth: number
+  bottomCap: Phaser.GameObjects.Image
+  middle: Phaser.GameObjects.Image
+  topCap: Phaser.GameObjects.Image
+  fullHeight: number
 
   constructor() {
     super({ key: 'MainScene' })
+    this.fullHeight = 300
   }
 
   create() {
@@ -13,13 +19,66 @@ export default class MainScene extends Phaser.Scene {
     this.fpsText = new FpsText(this)
 
     // display the Phaser.VERSION
+    
     this.add
       .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
         color: '#000000',
         fontSize: '24px'
       })
       .setOrigin(1, 0)
+
+    // bar
+
+    const y = this.cameras.main.height * 0.75
+    const x = this.cameras.main.width * 0.75
+
+    // background shadow
+
+    const bottomShadowCap = this.add.image(x, y, 'bottom-cap-shadow').setOrigin(0.5, 0)
+
+    const middleShadowCap = this.add.image(x, bottomShadowCap.y - this.fullHeight, 'middle-shadow').setOrigin(0.5, 0)
+
+    middleShadowCap.displayHeight = this.fullHeight
+
+    this.add.image(x, middleShadowCap.y, 'top-cap-shadow').setOrigin(0.5, 1)
+
+    // health bar
+
+    this.bottomCap = this.add.image(x, y, 'bottom-cap').setOrigin(0.5, 0)
+    
+    this.middle = this.add.image(this.bottomCap.x, this.bottomCap.y, 'middle').setOrigin(0.5, 0)
+
+    this.topCap = this.add.image(x, this.bottomCap.y - this.fullHeight, 'top-cap').setOrigin(0.5, 1)
+
+    //this.setMeterPercentage(0.75)
+    //this.setMeterPercentageAnimated(0)
   }
+
+  setMeterPercentage(percent = 1) {
+    const height = -this.fullHeight * percent
+    this.middle.displayHeight = height
+    this.topCap.y = this.middle.y - this.middle.displayHeight
+  }
+
+  // setMeterPercentageAnimated(percent = 1, duration = 1000) {
+  //   const width = this.fullWidth * percent
+
+  //   this.tweens.add({
+  //     targets: this.middle,
+  //     //displayWidth: width,
+  //     displayLength: length,
+  //     duration,
+  //     ease: Phaser.Math.Easing.Sine.Out,
+  //     onUpdate: () => {
+  //       this.rightCap.x = this.middle.x + this.middle.displayWidth
+
+  //       this.leftCap.visible = this.middle.displayWidth > 0
+  //       this.middle.visible = this.middle.displayWidth > 0
+  //       this.rightCap.visible = this.middle.displayWidth > 0
+  //     }
+  //   })
+  // }
+
 
   update() {
     this.fpsText.update()
